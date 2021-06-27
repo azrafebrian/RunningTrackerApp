912975aa614b0fe041814f39df989dcfb64ddcc7
henrikusw96 <henrikuswahyu007@gmail.com>
2021-06-27 20:49:49 +0700

    partially implemented map tracker, history

--- app/src/main/java/com/example/runningtrackerapp/MapsActivity.java
+++ app/src/main/java/com/example/runningtrackerapp/MapsActivity.java
@@ -1,146 +1,84 @@
 package com.example.runningtrackerapp;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.content.res.Resources;
+import android.database.Cursor;
+import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MapStyleOptions;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.PointOfInterest;
+import com.google.android.gms.maps.model.PolylineOptions;
+import java.util.ArrayList;
 
-import java.util.Locale;
-
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
-
-    private static final int REQUEST_LOCATION_PERMISSION = 1;
+/* Page to display journey of a user on the map */
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
     private GoogleMap mMap;
+    private long journeyID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        Bundle bundle = getIntent().getExtras();
+        journeyID = bundle.getLong("journeyID");
     }
 
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        // draw polyline
+        Cursor c = getContentResolver().query(JourneyProviderContract.LOCATION_URI,
+                null, JourneyProviderContract.L_JID + " = " + journeyID, null, null);
+
+        PolylineOptions line = new PolylineOptions().clickable(false);
+        LatLng firstLoc = null;
+        LatLng lastLoc = null;
         try {
-            boolean success = googleMap.setMapStyle(
-                    MapStyleOptions.loadRawResourceStyle(this,
-                            R.raw.map_style));
-            if (!success) {
-                Log.e("TAG", "Style parsing failed.");
-            }
-        } catch (Resources.NotFoundException e) {
-            Log.e("TAG", "Can't find style. Error: ", e);
-        }
-
-        float zoom = 15;
-        setMapLongClick(mMap);
-        setPoiClick(mMap);
-        enableMyLocation();
-    }
-
-    private void setMapLongClick(final GoogleMap map) {
-        map.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
-            @Override
-            public void onMapLongClick(LatLng latLng) {
-                String snippet = String.format(Locale.getDefault(),
-                        "Lat:%1$.3f, Lng:%2$.3f", latLng.latitude, latLng.longitude);
-                map.addMarker(new MarkerOptions().snippet(snippet)
-                        .position(latLng).title("Dropped Pin")
-                        .icon(BitmapDescriptorFactory.defaultMarker(
-                                BitmapDescriptorFactory.HUE_BLUE)));
-            }
-        });
-    }
-
-    private void setPoiClick(GoogleMap map) {
-        map.setOnPoiClickListener(new GoogleMap.OnPoiClickListener() {
-            @Override
-            public void onPoiClick(PointOfInterest pointOfInterest) {
-                Marker poiMarker = map.addMarker(new MarkerOptions()
-                        .position(pointOfInterest.latLng)
-                        .title(pointOfInterest.name));
-                poiMarker.showInfoWindow();
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.map_options, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.normal_map:
-                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-                return true;
-            case R.id.hybrid_map:
-                mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-                return true;
-            case R.id.satellite_map:
-                mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-                return true;
-            case R.id.terrain_map:
-                mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
-    private void enableMyLocation() {
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
-            mMap.setMyLocationEnabled(true);
-        } else {
-            ActivityCompat.requestPermissions(this, new String[]
-                            {Manifest.permission.ACCESS_FINE_LOCATION},
-                    REQUEST_LOCATION_PERMISSION);
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case REQUEST_LOCATION_PERMISSION:
-                if (grantResults.length > 0
-                        && grantResults[0]
-                        == PackageManager.PERMISSION_GRANTED) {
-                    enableMyLocation();
-                    break;
+            while(c.moveToNext()) {
+                LatLng loc = new LatLng(c.getDouble(c.getColumnIndex(JourneyProviderContract.L_LATITUDE)),
+                        c.getDouble(c.getColumnIndex(JourneyProviderContract.L_LONGITUDE)));
+                if(c.isFirst()) {
+                    firstLoc = loc;
                 }
+                if(c.isLast()) {
+                    lastLoc = loc;
+                }
+                line.add(loc);
+            }
+        } finally {
+            c.close();
         }
+
+        float zoom = 15.0f;
+        if(lastLoc != null && firstLoc != null) {
+            mMap.addMarker(new MarkerOptions().position(firstLoc).title("Start"));
+            mMap.addMarker(new MarkerOptions().position(lastLoc).title("End"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(firstLoc, zoom));
+        }
+        mMap.addPolyline(line);
     }
+
 }
