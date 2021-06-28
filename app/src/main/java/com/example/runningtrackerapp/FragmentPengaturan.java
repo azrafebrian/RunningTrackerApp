8f2b11bed95baa18782e035c975c571f73f929b0
Azra Febrian <azrafebrianm002@gmail.com>
2021-06-28 09:14:24 +0700

    Fix button (language)

--- app/src/main/java/com/example/runningtrackerapp/FragmentPengaturan.java
+++ app/src/main/java/com/example/runningtrackerapp/FragmentPengaturan.java
@@ -2,6 +2,7 @@ package com.example.runningtrackerapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,12 +26,21 @@ public class FragmentPengaturan extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         Button button = view.findViewById(R.id.logout);
+        Button language = view.findViewById(R.id.action_language);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 FirebaseAuth.getInstance().signOut();
                 startActivity(new Intent(requireActivity().getApplicationContext(), LoginActivity.class));
                 getActivity().finish();
+            }
+        });
+
+        language.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent langIntent=new Intent(Settings.ACTION_LOCALE_SETTINGS);
+                startActivity(langIntent);
             }
         });
     }
@@ -40,4 +50,5 @@ public class FragmentPengaturan extends Fragment {
         startActivity(new Intent(requireActivity().getApplicationContext(), LoginActivity.class));
         getActivity().finish();
     }
+
 }
